9e29880651d7e907503cd77f4406a63e590a63cd
Julian <93773032+war4tek@users.noreply.github.com>
2023-11-15 08:37:59 -0700

    Initial commit

---
+++ js/dice.js
@@ -0,0 +1,243 @@
+/**
+ * Julian Apodaca
+ * 11-09-2022
+ * Logic for a set of yahtzee dice. 
+ */
+
+const rollDiceBtn = document.getElementById('rollDiceBtn');
+const diceOne = document.getElementById('dice-one');
+const diceTwo = document.getElementById('dice-two');
+const diceThree = document.getElementById('dice-three');
+const diceFour = document.getElementById('dice-four');
+const diceFive  = document.getElementById('dice-five');
+const diceSix = document.getElementById('dice-six');
+const rollCount = document.getElementById('rollCount');
+const resetDiceBtn = document.getElementById('resetDiceBtn');
+
+let numberOfRolls = 0;
+
+const diceInfo = {
+    'first-face': '<span class="dot"></span>',
+    'second-face': `<span class="dot"></span>
+                    <span class="dot"></span>`,
+    'third-face': `<span class="dot"></span> 
+                   <span class="dot"></span>
+                   <span class="dot"></span>`,
+    'fourth-face': `<div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    <div class="column" >
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>`,
+     'fifth-face': `<div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+
+                    <div class="column">
+                     <span class="dot"></span>
+                    </div>
+
+                    <div class="column">
+                     <span class="dot"></span>
+                    <span class="dot"></span>
+                   </div>`,
+    'sixth-face': `<div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                  </div>
+
+                  <div class="column">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                 </div>` 
+};
+
+function getRandomNumber(max){
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function getFace(number){
+    if(number === 1) return 'first-face';
+    else if (number === 2) return 'second-face';
+    else if (number === 3) return 'third-face';
+    else if (number === 4) return 'fourth-face';
+    else if (number === 5) return 'fifth-face';
+    else if (number === 6) return 'sixth-face';
+}
+
+function animateDice(){
+    const divs = document.getElementsByClassName('dice');
+    for(div of divs){
+        if(div.className.includes('selected')){
+            div.classList.remove('play');
+        }else{
+            div.classList.add('play');
+        }
+    }
+}
+
+function resetAnimation(){  
+    const divs = document.getElementsByClassName('dice');
+    for(div of divs){
+        console.log(div.classList);
+        if(!div.className.includes('selected')){
+            div.style.animation = 'none';
+            div.offsetHeight;
+            div.style.animation = null;           
+            div.className = '';
+            div.style.backgroundColor = 'red';
+            div.classList.add('dice');
+            div.innerHTML = ``;
+        }
+
+        if(div.id.includes('six')){
+            div.classList.add('hide');
+            div.classList.add('smaller-dice');
+        }        
+    }
+}
+ 
+function displayDice(){
+    const divs = document.getElementsByClassName('dice');
+
+    for (div of divs){
+        let number = getRandomNumber(6);
+        let face = getFace(number);
+        
+        if(!div.className.includes('selected')){
+            if(face.includes("first")){
+                div.classList.add('first-face');
+                div.innerHTML = diceInfo["first-face"];
+            } 
+            else if(face.includes("second")){
+                div.classList.add('second-face');
+                div.innerHTML = diceInfo["second-face"];
+            }
+            else if(face.includes("third")){
+                div.classList.add('third-face');
+                div.innerHTML = diceInfo["third-face"];
+            }
+            else if(face.includes("fourth")){
+                div.classList.add('fourth-face');
+                div.innerHTML = diceInfo["fourth-face"];
+            }
+            else if(face.includes("fifth")){
+                div.classList.add('fifth-face');
+                div.innerHTML = diceInfo["fifth-face"];
+            }  
+            else if(face.includes("sixth")){
+                div.classList.add('sixth-face');
+                div.innerHTML = diceInfo["sixth-face"];
+            }  
+        }
+    }
+}
+
+function displayCount(){
+    rollCount.innerHTML = `Roll ${numberOfRolls}`;
+}
+
+function rollDice(){  
+    resetAnimation();
+    animateDice();
+    numberOfRolls++; 
+} 
+
+function toggleFirstDice(){
+    if(diceOne.style.backgroundColor === 'red'){
+        diceOne.style.backgroundColor = 'green';
+        diceOne.classList.add('selected');
+    }
+    else{
+        diceOne.style.backgroundColor = 'red';
+        diceOne.classList.remove('selected');
+    } 
+}
+
+function toggleSecondDice(){
+    if(diceTwo.style.backgroundColor === 'red'){
+        diceTwo.style.backgroundColor = 'green';
+        diceTwo.classList.add('selected');  
+    }
+    else{
+        diceTwo.style.backgroundColor = 'red';
+        diceTwo.classList.remove('selected');
+    } 
+}
+
+function toggleThirdDice(){
+    if(diceThree.style.backgroundColor === 'red'){
+        diceThree.style.backgroundColor = 'green'; 
+        diceThree.classList.add('selected'); 
+    }
+    else{
+        diceThree.style.backgroundColor = 'red';
+        diceThree.classList.remove('selected');
+    } 
+}
+
+function toggleFourthDice(){
+    if(diceFour.style.backgroundColor === 'red'){
+        diceFour.style.backgroundColor = 'green'; 
+        diceFour.classList.add('selected'); 
+    }
+    else{
+        diceFour.style.backgroundColor = 'red';
+        diceFour.classList.remove('selected');
+    } 
+}
+
+function toggleFifthDice(){
+    if(diceFive.style.backgroundColor === 'red'){
+        diceFive.style.backgroundColor = 'green';
+        diceFive.classList.add('selected');  
+    }
+    else{
+        diceFive.style.backgroundColor = 'red';
+        diceFive.classList.remove('selected');
+    } 
+}
+
+function resetHand(){
+    const divs = document.getElementsByClassName('dice');
+    for(div of divs){
+        div.style.animation = 'none';
+        div.offsetHeight;
+        div.style.animation = null;           
+        div.className = '';
+        div.style.backgroundColor = 'red';
+        div.classList.add('dice');
+        div.innerHTML = ``;
+    }
+
+    if(div.id.includes('six')){
+        div.classList.add('hide');
+        div.classList.add('smaller-dice');
+    }
+}
+
+rollDiceBtn.addEventListener('click', rollDice);
+
+diceOne.addEventListener('click', toggleFirstDice);
+diceTwo.addEventListener('click', toggleSecondDice);
+diceThree.addEventListener('click', toggleThirdDice);
+diceFour.addEventListener('click', toggleFourthDice);
+diceFive.addEventListener('click', toggleFifthDice);
+
+diceSix.addEventListener("animationend", ()=> {
+    displayDice();
+    displayCount();
+});
+
+resetDiceBtn.addEventListener('click', ()=> {
+    resetHand();
+    numberOfRolls = 0; 
+    rollCount.innerHTML = ``;
+});
+
+rollDiceBtn.focus();
---
+++ js/game.js
@@ -0,0 +1,176 @@
+/**
+ * Julian Apodaca
+ * 11-09-2022
+ * Digital score card for Yahtzee
+ */
+
+const yahtzeeDiv = document.getElementById('yahtzee');
+const onesInput = document.getElementById('ones-input');
+const twosInput = document.getElementById('twos-input');
+const threesInput = document.getElementById('threes-input');
+const foursInput = document.getElementById('fours-input');
+const fivesInput = document.getElementById('fives-input');
+const sixesInput = document.getElementById('sixes-input');
+
+const upperTotalInput = document.getElementById('upper-total-input');
+const upperGrandTotalInput = document.getElementById('upper-grandtotal-input');
+const bonusInput = document.getElementById('bonus-input');
+
+const threeKindInput = document.getElementById('three-kind-input');
+const fourKindInput = document.getElementById('four-kind-input');
+const fullHouseInput = document.getElementById('full-house-input');
+const smallInput = document.getElementById('small-straight-input');
+const largeInput = document.getElementById('large-straight-input');
+const yahtzeeInput = document.getElementById('yahtzee-input');
+const chanceInput = document.getElementById('chance-input');
+
+const lowerTotalBottomInput = document.getElementById('lower-total-bottom-input');
+const upperTotalBottomInput = document.getElementById('upper-total-bottom-input');
+const grandTotalBottomInput = document.getElementById('grand-total-bottom-input');
+const bonusDivContainer = document.getElementById('bonus-div-container');
+const yahtzeeBonusTotalInput = document.getElementById('yahtzee-bonus-bottom-input');
+
+const firstBonusDiv = document.getElementById('first-bonus-div');
+const firstBonus = document.getElementById('first-bonus');
+const secondBonusDiv = document.getElementById('second-bonus-div');
+const secondBonus = document.getElementById('second-bonus');
+const thirdBonusDiv = document.getElementById('third-bonus-div');
+const thirdBonus = document.getElementById('third-bonus');
+
+const resetCardBtn = document.getElementById('resetCardBtn');
+const saveGameBtn = document.getElementById('saveGameBtn');
+
+let upperGrandTotal = 0;
+let bonusYahtzee = 0;
+let gameOver = false;
+
+
+
+function calculateUpperSection(){
+    let ones = Number(onesInput.value);
+    let twos = Number(twosInput.value);
+    let threes = Number(threesInput.value);
+    let fours = Number(foursInput.value);
+    let fives = Number(fivesInput.value);
+    let sixes = Number(sixesInput.value);
+
+    let bonus = 35;
+
+    let upperTotal = ones + twos + threes + fours + fives + sixes;
+    upperTotalInput.value = upperTotal;
+
+    if (upperTotal >= 63) {
+        upperGrandTotal = upperTotal + bonus;
+        bonusInput.value = bonus;
+    }
+    else{
+        upperGrandTotal = upperTotal;
+    }
+
+    upperGrandTotalInput.value = upperGrandTotal;
+}
+
+
+function calculateLowerSection(){
+    let threeKind = Number(threeKindInput.value);
+    let fourKind = Number(fourKindInput.value);
+    let fullHouse = Number(fullHouseInput.value);
+    let smallStraight = Number(smallInput.value);
+    let largeStraight = Number(largeInput.value);
+    let yahtzee = Number(yahtzeeInput.value);
+    let chance = Number(chanceInput.value);
+    let upperTotal = Number(upperTotalBottomInput.value);
+
+    let lowerTotal = threeKind+fourKind+fullHouse+smallStraight+largeStraight+yahtzee+chance+bonusYahtzee;
+    
+    let grandTotal = lowerTotal + upperGrandTotal;
+
+    lowerTotalBottomInput.value = lowerTotal;
+    upperTotalBottomInput.value = upperGrandTotal;
+    yahtzeeBonusTotalInput.value = bonusYahtzee;
+    grandTotalBottomInput.value = grandTotal ;
+}
+
+function calculateScore(){
+    calculateUpperSection();
+    calculateLowerSection();
+}
+
+onesInput.addEventListener('input', calculateScore);
+twosInput.addEventListener('input', calculateScore);
+threesInput.addEventListener('input', calculateScore);
+foursInput.addEventListener('input', calculateScore);
+fivesInput.addEventListener('input', calculateScore);
+sixesInput.addEventListener('input', calculateScore);
+
+threeKindInput.addEventListener('input', calculateScore);
+fourKindInput.addEventListener('input', calculateScore);
+fullHouseInput.addEventListener('input', calculateScore);
+smallInput.addEventListener('input', calculateScore);
+largeInput.addEventListener('input', calculateScore);
+yahtzeeInput.addEventListener('input', calculateScore);
+chanceInput.addEventListener('input', calculateScore);
+
+lowerTotalBottomInput.addEventListener('input', calculateScore);
+upperTotalBottomInput.addEventListener('input', calculateScore);
+grandTotalBottomInput.addEventListener('input', calculateScore);
+
+firstBonusDiv.addEventListener('click', ()=> {
+    if(firstBonus.classList.contains('hide')){
+        firstBonus.classList.remove('hide');
+        bonusYahtzee += 100;
+    }else{
+        firstBonus.classList.add('hide');
+        bonusYahtzee -= 100;
+    }
+    yahtzeeBonusTotalInput.value = bonusYahtzee;
+    calculateScore();
+});
+
+secondBonusDiv.addEventListener('click', () => {
+    if(secondBonus.classList.contains('hide')){
+        secondBonus.classList.remove('hide');
+        bonusYahtzee += 100;
+    }else{
+        secondBonus.classList.add('hide');
+        bonusYahtzee -= 100;
+    }
+    yahtzeeBonusTotalInput.value = bonusYahtzee;
+    calculateScore();
+});
+
+thirdBonusDiv.addEventListener('click', () =>{
+    if(thirdBonus.classList.contains('hide')){
+        thirdBonus.classList.remove('hide');
+        bonusYahtzee += 100;
+    }else{
+        thirdBonus.classList.add('hide');
+        bonusYahtzee -= 100;
+    }
+    yahtzeeBonusTotalInput.value = bonusYahtzee;
+    calculateScore();
+});
+
+resetCardBtn.addEventListener('click', ()=> {
+    const inputFields = document.getElementsByTagName('input');
+    for(inputField of inputFields){
+        inputField.value = ``;
+    }
+
+    upperTotalInput.value = 0;
+    upperGrandTotalInput.value = 0;
+
+    yahtzeeBonusTotalInput.value = 0;
+    lowerTotalBottomInput.value = 0;
+    upperTotalBottomInput.value = 0;
+    grandTotalBottomInput.value = 0;
+
+    firstBonus.classList.add('hide');
+    secondBonus.classList.add('hide');
+    thirdBonus.classList.add('hide');
+
+    bonusYahtzee = 0;
+
+});
+
+calculateScore();
